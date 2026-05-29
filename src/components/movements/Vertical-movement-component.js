2de4d73f19f16911b0aa9a5ffc3b3245702e178d
DevFlavioFernandes <dev.flaviofernandes@gmail.com>
2026-05-29 23:39:14 +0100

    Add enemies - Basic and Wave
    Create the first enemies for the game.
    Start by building the Scout enemy class and add a new component for vertical movement. Then add a second type of enemy that have like a wave pattern for left and right in the screen

---
+++ src/components/movements/Vertical-movement-component.js
@@ -0,0 +1,33 @@
+import * as CONFIG from '../../config.js';
+
+export class VerticalMovementComponent {
+    #gameObject;
+    #InputComponent;
+    #velocity;
+
+    constructor (gameObject, inputComponent, velocity) {
+        this.#gameObject = gameObject;
+        this.#InputComponent = inputComponent;
+        this.#velocity = velocity;
+
+        this.#gameObject.body.setDamping(true);
+        this.#gameObject.body.setDrag(CONFIG.COMPONENT_MOVEMENT_VERTICAL_DRAG);
+        this.#gameObject.body.setMaxVelocity(CONFIG.COMPONENT_MOVEMENT_VERTICAL_MAX_VELOCITY);
+    }
+
+    reset() {
+        this.#gameObject.body.velocity.y = 0;
+        this.#gameObject.body.setAngularAcceleration(0);
+    }
+
+    update() { 
+        if (this.#InputComponent.downIsDown) {
+            this.#gameObject.body.velocity.y += this.#velocity;   
+        }else if (this.#InputComponent.upIsDown) {
+            this.#gameObject.body.velocity.y -= this.#velocity;   
+        }else {
+            this.#gameObject.body.setAngularAcceleration(0);   
+        }
+}
+
+}
